4eb05d69240d6f2e3177ee341904a22619a9b37a
Farhat Zafar <farhatzafar63@gmail.com>
2025-02-10 11:43:42 -0500

    add Spring boot initialized code

---
+++ whalewatch/whalewatch/src/main/java/com/quebec/whalewatch/WhalewatchApplication.java
@@ -0,0 +1,13 @@
+package com.quebec.whalewatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhalewatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhalewatchApplication.class, args);
+	}
+
+}
---
+++ whalewatch/whalewatch/src/test/java/com/quebec/whalewatch/WhalewatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quebec.whalewatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhalewatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
